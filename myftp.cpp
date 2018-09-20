970e7623f59a5f3cf9cce14c4e3b2c63b69ca535
Jessica Nicole Hardey <jhardey@student01.cse.nd.edu>
2018-09-20 17:11:48 -0400

    myftp.cpp & myftp.h

--- myftp.cpp
+++ myftp.cpp
@@ -4,20 +4,6 @@ Name: Josefa Osorio, Herman Tong, Jessica Hardey
 Netid: josorio2, ktong1
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <string>
-#include <typeinfo>
-#include <iostream>
-#include <sys/time.h>
-#include <unistd.h>
-
 int main(int argc, char* argv[])
 {
     char *host;
---
+++ myftp.h
@@ -0,0 +1,19 @@
+/*
+File: myftp.cpp
+Name: Josefa Osorio, Herman Tong, Jessica Hardey
+Netid: josorio2, ktong1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <sys/time.h>
+#include <unistd.h>
